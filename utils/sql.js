8456fd72d9ac59b4f40200cd43529fc293d9a83e
Joirid Juárez <a00835769@tec.mx>
2025-02-27 13:00:12 -0600

    Actualización backend

--- utils/sql.js
+++ utils/sql.js
@@ -1,4 +1,4 @@
-import sql from 'mssql'
+import sql from "mssql";
 
 const sqlConfig = {
   user: process.env.DB_USER,
@@ -7,7 +7,7 @@ const sqlConfig = {
   server: process.env.DB_SERVER,
   options: {
     encrypt: true, // for azure
-    trustServerCertificate: false // change to true for local dev / self-signed certs
+    trustServerCertificate: true,// change to true for local dev / self-signed certs
   },
 };
 
