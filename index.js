9b24b6259d6415077cb459aeb0d1c81541379d51
Joirid Juárez <a00835769@tec.mx>
2025-03-06 12:29:39 -0600

    Conexión con MongoDB

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ import cors from "cors";
 import morgan from "morgan";
 import indexRoutes from "./routes/index.routes.js";
 import itemsRoutes from "./routes/items.routes.js";
+import items2Routes from "./routes/items2.routes.js";
 import loginRoutes from "./routes/login.routes.js";
+import { connectDB } from "./utils/mogodb.js";
 
 const app = express();
+
+connectDB();
+
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(indexRoutes);
 app.use(itemsRoutes);
+app.use(items2Routes);
 app.use(loginRoutes);
 
 app.listen(5001, console.log("http://localhost:5001"));
